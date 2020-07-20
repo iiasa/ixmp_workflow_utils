f1f95d9181eac7b2fd0ab4cbeadb7ab27c441732
Nikolay Kushin <zikolach@gmail.com>
2020-07-20 10:45:27 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ixmp_workflow_utils',
+    version='0.0.1',
+    url='https://github.com/iiasa/ixmp_server_integration.git',
+    author='Kushin Nikolay',
+    author_email='kushin@iiasa.ac.at',
+    description='Common utilities to use in application workflow scripts',
+    packages=['ixmp_workflow_utils'],
+    install_requires=[
+    ],
+)
